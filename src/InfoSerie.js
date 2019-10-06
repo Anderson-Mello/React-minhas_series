d5255a8cd1c46d8f3f6e1439a5ec9083f0509695
AndersonMello13 <anderson_mello97@hotmail.com>
2019-10-06 12:47:32 -0300

    Info series done

--- src/InfoSerie.js
+++ src/InfoSerie.js
@@ -4,10 +4,13 @@ import { Redirect } from 'react-router-dom'
 import { Badge } from 'reactstrap'
 
 const InfoSerie = ({ match }) => {
-    const [form, setForm] = useState({})
+    const [form, setForm] = useState({
+        name: ''
+    })
     const [success, setSuccess] = useState(false) //No success yet
-    const [mode, setMode] = useState('EDIT')
+    const [mode, setMode] = useState('INFO')
     const [genre, setGenre] = useState([])
+    const [genreId, setGenreId] = useState('')
 
     const [data, setData] = useState({})
 
@@ -25,8 +28,13 @@ const InfoSerie = ({ match }) => {
             .get('/api/genres/')
             .then(res => {
                 setGenre(res.data.data)
+                const genres = res.data.data
+                const found = genres.find(value => data.genre === value.name) 
+                if (found) {
+                    setGenreId(found.id)
+                }
             })
-    }, []) //Without dependences
+    }, [data])
 
     //custom header
     const masterHeader = {
@@ -36,6 +44,10 @@ const InfoSerie = ({ match }) => {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat'
+    }
+
+    const onChangeGenre = evt => {
+        setGenreId(evt.target.value)
     }
 
     const onChange = field => evt => {
@@ -54,14 +66,17 @@ const InfoSerie = ({ match }) => {
 
     const save = () => {
         axios
-            .put('/api/series/' + match.params.id, form)
+            .put('/api/series/' + match.params.id, {
+                ...form,
+                genre_id: genreId
+            })
             .then(res => {
                 setSuccess(true)
             })
     }
 
     if (success) {
-        //return <Redirect to='/series' />
+        return <Redirect to='/series' />
     }
 
     return (
@@ -76,8 +91,8 @@ const InfoSerie = ({ match }) => {
                             <div className='col-8'>
                                 <h1 className='font-weight-light text-white'>{data.name}</h1>
                                 <div className='lead text-white'>
-                                    <Badge color='success'>Watched</Badge>
-                                    <Badge color='warning'>To watch</Badge>
+                                    { data.status === 'WATCHED' && <Badge color='success'>Watched</Badge> }
+                                    { data.status === 'TO_WATCH' && <Badge color='warning'>To watch</Badge> }
                                     <br />Genre: {data.genre}
                                 </div>
                             </div>
@@ -89,7 +104,7 @@ const InfoSerie = ({ match }) => {
             {
                 mode === 'INFO' &&
 
-                <div>
+                <div class='container'>
                     <button className='btn btn-primary' onClick={() => setMode('EDIT')}>Edit</button>
                 </div>
             }
@@ -99,7 +114,6 @@ const InfoSerie = ({ match }) => {
 
                 <div className='container'>
                     <h1>Edit Serie</h1>
-                    <pre>{JSON.stringify(form)}</pre>
                     <div>
                         <button className='btn btn-danger' onClick={() => setMode('INFO')}>Cancel edit</button>
                     </div>
@@ -110,22 +124,22 @@ const InfoSerie = ({ match }) => {
                         </div>
                         <div className='form-group'>
                             <label htmlFor='comments'>Comments</label>
-                            <input type='text' value={form.comments} onChange={onChange('comments')} className='form-control' id='comments ' placeholder='Comments' />
+                            <input type='text' value={form.comments} onChange={onChange('comments')} className='form-control' id='comments' placeholder='Comments' />
                         </div>
                         <div className='form-group'>
                             <label htmlFor='genre'>Genre</label>
-                            <select className='form-control' onChange={onChange('genre_id')}>
-                                {genre.map(genre => <option key={genre.id} value={genre.id} select={genre.id === form.genre}>{genre.name}</option>)}
+                            <select className='form-control' onChange={onChangeGenre} value={genreId}>
+                                {genre.map(genre => <option key={genre.id} value={genre.id}>{genre.name}</option>)}
                             </select>
                         </div>
                         <div className='form-check'>
-                            <input className='form-check-input' type='radio' name='status' id='watched' value='WATCHED' onClick={selectStatus('WATCHED')}/>
+                            <input className='form-check-input' type='radio' checked={form.status === 'WATCHED'} name='status' id='watched' value='WATCHED' onChange={selectStatus('WATCHED')}/>
                             <label className='form-check-label' htmlFor='watched'>
                                 Watched
                             </label>
                         </div>
                         <div className='form-check'>
-                            <input className='form-check-input' type='radio' name='status' id='toWatch' value='TO_WATCH' onClick={selectStatus('TO_WATCH')}/>
+                            <input className='form-check-input' type='radio' checked={form.status === 'TO_WATCH'} name='status' id='toWatch' value='TO_WATCH' onChange={selectStatus('TO_WATCH')}/>
                             <label className='form-check-label' htmlFor='toWatch'>
                                 To watch
                             </label>
